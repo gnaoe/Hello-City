abffd8b6a68cffeaf18254a9e84ce6b1b93e07d1
gnaoe <egnaot@gmail.com>
2021-04-15 09:52:50 +0000

    Remove duplication by using app layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,18 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-        <title>Abour Us | Hello City</title>
-    </head>
-    <body>
+@section('content')
+    
         <p>Built with &hearts; by les TEACHERS DU NET.</p>
+        
         <P><a href="/">Revenir à la page d'accueil.</a></P>
 
-        <footer>
-            &copy; Copywright {{date('Y')}} &middot;
-        </footer>
-
-    </body>
-</html>
+@endsection
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,19 +1,9 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('app')
 
-        <title>Hello City</title>
-    </head>
-    <body>
+@section('content')
+    
         <h1>Hello from Abidjan!</h1>
         
         <p>It's currently {{date('h:i A')}}.</p>
 
-        <footer>
-            &copy; Copywright {{date('Y')}} &middot; <a href="/about-us">About Us</a>
-        </footer>
-
-    </body>
-</html>
+@endsection
